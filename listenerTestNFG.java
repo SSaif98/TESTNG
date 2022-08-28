909d46b265e100a95506f0f588b6e4ab575cda78
SSaif98 <57060514+SSaif98@users.noreply.github.com>
2022-08-28 14:23:09 +0500

    Add files via upload

---
+++ listenerTestNFG.java
@@ -0,0 +1,50 @@
+package test;
+
+import org.testng.ITestContext ;		
+import org.testng.ITestListener ;		
+import org.testng.ITestResult ;	
+
+//ITestListner interface which implements testng listner
+public class listenerTestNFG implements ITestListener {
+	  @Override		
+	    public void onFinish(ITestContext arg0) {					
+	        // TODO Auto-generated method stub				
+	        		
+	    }		
+
+	    @Override		
+	    public void onStart(ITestContext arg0) {					
+	        // TODO Auto-generated method stub				
+	        		
+	    }		
+
+	    @Override		
+	    public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {					
+	        // TODO Auto-generated method stub				
+	        		
+	    }		
+
+	    @Override		
+	    public void onTestFailure(ITestResult result) {					
+	        // TODO Auto-generated method stub				
+    		System.out.println("failed "  + result.getName());
+	    }
+
+	    @Override		
+	    public void onTestSkipped(ITestResult arg0) {					
+	        // TODO Auto-generated method stub				
+	        		
+	    }		
+
+	    @Override		
+	    public void onTestStart(ITestResult arg0) {					
+	        // TODO Auto-generated method stub				
+	        		
+	    }		
+
+	    @Override		
+	    public void onTestSuccess(ITestResult arg0) {					
+	        // TODO Auto-generated method stub				
+	        		System.out.println("passed");
+	    }		
+}
